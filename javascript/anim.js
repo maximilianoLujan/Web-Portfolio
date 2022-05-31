be46a2571a63fbd436ce793dafcad4a6c04f2062
Maximiliano <maxylujan1@outlook.com>
2022-05-31 18:28:08 -0300

    Agregandos el direccionamiento a las redes sociales, asi como a descargar el cv

--- javascript/anim.js
+++ javascript/anim.js
@@ -11,5 +11,12 @@ function funcionCerrar(){
     $botoncerrar.style.display ="none";
     $botonmenu.style.display ="block";
 }
-$botonmenu.addEventListener("click",funcionBoton);
-$botoncerrar.addEventListener("click",funcionCerrar);
+document.addEventListener("click",e=>{
+    if (e.target.className === "cerrar--boton") funcionCerrar();
+    if (e.target.className === "menu--boton") funcionBoton();
+    if (e.target.className === "links github") window.open("https://github.com/maximilianoLujan");
+    if (e.target.className === "links linkedln") window.open("https://www.linkedin.com/in/maximiliano-luj%C3%A1n-13323220a/");
+    if (e.target.className === "links twitter") window.open("https://twitter.com/MaximilianoL01");
+    if (e.target.className === "links instagram") window.open("https://www.instagram.com/maxilujan01/");
+    if (e.target.className === "español") window.open("../CV/MaximilianoLujan(es)-cv.pdf");
+})
